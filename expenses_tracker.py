2751d8fcf406158110413e3d3aa4112bb98d5bd1
ajeetsharma96 <ajeetsharma500045@gmail.com>
2025-12-05 17:40:38 +0530

    function "get_user_expenses" is defined now

--- expenses_tracker.py
+++ expenses_tracker.py
@@ -22,3 +22,34 @@ def main():
     # Read file and summarize expense.
      summarize_expense(expense_file_path,budget)
  
+
+def get_user_expense():
+    print(f"Getting user expense: ")
+
+    # expense name
+    expense_name=input("Enter expense name: ")
+    print(f"You have entered {expense_name} ")
+    
+    # expense amount
+    expense_amount=float(input("Enter expense amount: "))
+
+    # expense category
+    expense_categories=[ 
+        "Food","Home","Work","Fun","Miscellaneous","Shopping"
+    ]
+    
+    while True:
+        print("Select a category: ")
+        for i,category_name in enumerate(expense_categories):
+            print(f"{i+1}.{category_name}")
+        value_range=f"[1 - {len(expense_categories)}]"
+        selected_index=int(input(f"Enter a category number {value_range}: "))-1
+        
+        if selected_index in range(len(expense_categories)):
+            selected_category=expense_categories[selected_index]
+            new_expense= Expense(name=expense_name,category=selected_category,amount=expense_amount)
+            return new_expense
+        else:
+            print("Invalid category. Please try again!")
+
+        break
