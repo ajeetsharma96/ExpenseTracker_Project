a83c46558b8bec010e67a4618cadf646b8a341df
ajeetsharma96 <ajeetsharma500045@gmail.com>
2025-12-05 17:43:37 +0530

    function "Summarize_expenses" & "green" is defined

--- expenses_tracker.py
+++ expenses_tracker.py
@@ -58,3 +58,63 @@ def save_expense_to_file(expense: Expense,expense_file_path):
     print(f"Saving user Expense: {expense} to {expense_file_path}")
     with open(expense_file_path,"a") as f:
         f.write(f"{expense.name},{expense.amount},{expense.category}\n")
+
+
+def summarize_expense(expense_file_path,budget):
+    print(f"Summarize User Expense.")
+    expenses: List[Expense] = []
+
+    with open(expense_file_path,"r") as f:
+        lines=f.readlines()
+        for line in lines:
+            expense_name,expense_amount,expense_category=line.strip().split(",")
+            line_expense= Expense(
+                name=expense_name,
+                amount=float(expense_amount),
+                category=expense_category,
+            )
+            expenses.append(line_expense)
+
+        amount_by_category = {}
+        for expense in expenses:
+            key=expense.category
+            if key in amount_by_category:
+                amount_by_category[key] += expense.amount
+            else:
+                amount_by_category[key] = expense.amount
+
+        print("Expenses by Category: ")
+        for key,amount in amount_by_category.items():
+            print(f"  {key}: ${amount: .2f}")
+
+        total_spent = sum([ex.amount for ex in expenses])
+        print(f"You've spent ${total_spent: .2f} this month!")
+
+        remaining_budget=budget - total_spent
+        print(f"Budget remaining: ${remaining_budget: .2f}")
+
+        now=datetime.datetime.now()
+        days_in_month=calendar.monthrange(now.year,now.month)[1]
+        remaining_days=days_in_month - now.day  
+
+        daily_budget=remaining_budget/remaining_days
+        print(green(f"Budget Per Day: ${daily_budget: .2f}"))
+
+def green(text):
+        return f"\033[92m{text}\033[0m"
+ 
+        lines= f.readlines()
+        for line in lines:
+            expense_name,expense_amount,expense_category=line.strip().split(",")
+            line_expense=Expense(name=expense_name,amount=float(expense_amount),category=expense_category)
+            expenses.append(line_expense)
+        print(expenses)
+             
+
+
+
+
+
+
+if __name__=="__main__" :
+    main()
